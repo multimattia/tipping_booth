f53463c17ce745cc755028bfbfb099fbe4c6fc28
Matthew C. <mattdev23@protonmail.com>
2024-12-28 22:00:17 -0500

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,9 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Tipping Booth for testing compile times",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(tipping_booth::TemplateApp::new(cc)))),
     )
 }
 
@@ -50,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(tipping_booth::TemplateApp::new(cc)))),
             )
             .await;
 
